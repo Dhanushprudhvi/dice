56ae2a1fc1f2b3d2daa415199e2ece38a85c78d6
Dhanushprudhvi <87536182+Dhanushprudhvi@users.noreply.github.com>
2022-06-19 23:41:23 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -87,14 +87,14 @@ switch(val2)
 
 if(val1>val2)
 {
-    document.querySelector('h1').innerHTML='player1 wins';
+    document.querySelector('h1').innerHTML='you won';
     document.querySelector('.dice1 .text').innerHTML='<h3>you<br>('+val1+')</br></h3>';
     document.querySelector('.dice2 .text').innerHTML='<h3>computer<br>('+val2+')</br></h3>';
 
 }
 else if(val1<val2)
 {
-    document.querySelector('h1').innerHTML='player2 wins';
+    document.querySelector('h1').innerHTML='computer won';
     document.querySelector('.dice1 .text').innerHTML='<h3>you<br>('+val1+')</br></h3>';
     document.querySelector('.dice2 .text').innerHTML='<h3>computer<br>('+val2+')</br></h3>';
 }
